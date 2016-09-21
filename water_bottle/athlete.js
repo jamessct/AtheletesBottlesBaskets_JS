ed6f447e8df00f5b45ae657e93c70c52444654fa
James Scott <jamessct@hotmail.co.uk>
2016-09-21 09:18:54 +0100

    fixed homework

--- water_bottle/athlete.js
+++ water_bottle/athlete.js
@@ -7,14 +7,21 @@
 // 4. Athlete should not move when running dehydrated: hydration at 0;
 // 5. Athlete should be able to increase hydration by drinking from water bottle
 
-var athlete = require('./water_bottle');
+var waterBottle = require('./water_bottle');
 
 var athlete = {
   hydration: 100,
   distanceCovered: 0,
   run: function() {
+    if(this.hydration <= 0) {
     this.hydration -= 10;
-    this.distanceCovered +=10
+    this.hydration += 10;
+    this.distanceCovered += 10;
+    this.distanceCovered -= 10
+  } else {
+    this.hydration -= 10;
+    this.distanceCovered += 10
+  }
   },
   drink: function() {
     this.hydration += 10;
